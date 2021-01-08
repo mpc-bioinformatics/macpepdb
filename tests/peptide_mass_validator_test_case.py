0b9b67aaccf75a5bee74ca26512ea811f46e7056
Dirk Winkelhardt <dirk.winkelhardt@rub.de>
2021-01-08 13:37:10 +0100

    create peptide validator to check if a modified peptide matches a precursor

---
+++ tests/peptide_mass_validator_test_case.py
@@ -0,0 +1,134 @@
+import unittest
+import pathlib
+
+from trypperdb.models.peptide import Peptide
+from trypperdb.proteomics.modification import Modification, ModificationPosition
+from trypperdb.proteomics.amino_acid import AminoAcid
+from trypperdb.proteomics.mass.convert import to_int as mass_to_int
+from trypperdb.proteomics.mass.precursor_range import PrecursorRange
+from trypperdb.proteomics.modification_collection import ModificationCollection
+from trypperdb.peptide_mass_validator import PeptideMassValidator
+
+# Leptin peptide with 2 missed cleavages with a weigh of 5818137657950 (5818.137657950 Da)
+LEPTIN_PEPTIDE_SEQUENCE = "DJJHJJAASKSCPJPQVRAJESJESJGVVJEASJYSTEVVAJSRJQGSJQDMJR"
+
+class PeptideMassValidatorTestCase(unittest.TestCase):
+    def test_validation(self):
+        static_carbamidomethylation_of_c = Modification('unimod:4', 'carbamidomethylation of cysteine', AminoAcid.get_by_one_letter_code('C'), mass_to_int(57.021464), True, ModificationPosition.ANYWHERE)
+        variable_oxidation_of_m = Modification('unimod:35', 'oxidation of methionine', AminoAcid.get_by_one_letter_code('M'), mass_to_int(15.994915), False, ModificationPosition.ANYWHERE)
+        static_custom_modification_of_n_terminal_d = Modification('custom:1', 'custom of aspartic acid', AminoAcid.get_by_one_letter_code('D'), mass_to_int(10.01541), True, ModificationPosition.N_TERMINUS)
+        variable_custom_modification_of_n_terminal_d = Modification('custom:2', 'custom of aspartic acid', AminoAcid.get_by_one_letter_code('D'), mass_to_int(10.01541), False, ModificationPosition.N_TERMINUS)
+        static_custom_modification_of_c_terminal_r = Modification('custom:3', 'custom of arginine', AminoAcid.get_by_one_letter_code('R'), mass_to_int(6.153215), True, ModificationPosition.C_TERMINUS)
+        variable_custom_modification_of_c_terminal_r = Modification('custom:4', 'custom of arginine', AminoAcid.get_by_one_letter_code('R'), mass_to_int(6.153215), False, ModificationPosition.C_TERMINUS)
+
+        peptide = Peptide(LEPTIN_PEPTIDE_SEQUENCE, 2)
+
+        # Static carbamidomethylation of C
+        expected_peptide_mass = peptide.weight + peptide.c_count * static_carbamidomethylation_of_c.delta
+        modification_collection = ModificationCollection([static_carbamidomethylation_of_c])
+        precursor_range = PrecursorRange(expected_peptide_mass, 0, 0)
+        validator = PeptideMassValidator(modification_collection, 0, precursor_range)
+        self.assertTrue(validator.validate(peptide))
+
+        # This should als match with allowed variable modification (where actually none is applied)
+        # Static carbamidomethylation of C
+        # Variable oxidation of M (not considered in expected_weight)
+        modification_collection = ModificationCollection([static_carbamidomethylation_of_c, variable_oxidation_of_m])
+        validator = PeptideMassValidator(modification_collection, 3, precursor_range)
+        self.assertTrue(validator.validate(peptide))
+
+
+        # Static carbamidomethylation of C
+        # 1 variable oxidation of M
+        expected_peptide_mass = peptide.weight \
+            + peptide.c_count * static_carbamidomethylation_of_c.delta \
+            + 1 * variable_oxidation_of_m.delta
+        modification_collection = ModificationCollection([static_carbamidomethylation_of_c, variable_oxidation_of_m])
+        precursor_range = PrecursorRange(expected_peptide_mass, 0, 0)
+        validator = PeptideMassValidator(modification_collection, 3, precursor_range)
+        self.assertTrue(validator.validate(peptide))
+
+        # This should not match if no variable modifiations are allowed
+        # Static carbamidomethylation of C
+        # Variable oxidation of M (considered in expected_weight but no variable modification allowed in validation)
+        validator.set_maximum_number_of_variable_modifications(0)
+        self.assertFalse(validator.validate(peptide))
+
+        # Lets replace two Js with Ms and test 3 applied variable oxidations of M
+        # Static carbamidomethylation of C
+        # 3 Variable oxidation of M
+        peptide = Peptide(LEPTIN_PEPTIDE_SEQUENCE.replace('J', 'M', 2), 2)
+        expected_peptide_mass = peptide.weight \
+            + peptide.c_count * static_carbamidomethylation_of_c.delta \
+            + 3 * variable_oxidation_of_m.delta
+        modification_collection = ModificationCollection([static_carbamidomethylation_of_c, variable_oxidation_of_m])
+        precursor_range = PrecursorRange(expected_peptide_mass, 0, 0)
+        validator = PeptideMassValidator(modification_collection, 3, precursor_range)
+        self.assertTrue(validator.validate(peptide))
+
+        # This should fail with only 2 allowed variable modifications
+        validator.set_maximum_number_of_variable_modifications(2)
+        self.assertFalse(validator.validate(peptide))
+
+
+        # Test variable n-terminal
+        # Variable n-terminal modification of D
+        # Static carbamidomethylation of C
+        # 2 variable oxidation of M
+        expected_peptide_mass = peptide.weight \
+            + variable_custom_modification_of_n_terminal_d.delta \
+            + peptide.c_count * static_carbamidomethylation_of_c.delta \
+            + 2 * variable_oxidation_of_m.delta
+        modification_collection = ModificationCollection([static_carbamidomethylation_of_c, variable_oxidation_of_m, variable_custom_modification_of_n_terminal_d])
+        precursor_range = PrecursorRange(expected_peptide_mass, 0, 0)
+        validator = PeptideMassValidator(modification_collection, 3, precursor_range)
+        self.assertTrue(validator.validate(peptide))
+
+        # This should fail with only 2 allowed variable modifications
+        validator.set_maximum_number_of_variable_modifications(2)
+        self.assertFalse(validator.validate(peptide))
+
+
+        # Test static n-terminal modification
+        # Static n-terminal modification of D
+        # Static carbamidomethylation of C
+        # 2 variable oxidation of M
+        expected_peptide_mass = peptide.weight \
+            + static_custom_modification_of_n_terminal_d.delta \
+            + peptide.c_count * static_carbamidomethylation_of_c.delta \
+            + 2 * variable_oxidation_of_m.delta
+        modification_collection = ModificationCollection([static_carbamidomethylation_of_c, variable_oxidation_of_m, static_custom_modification_of_n_terminal_d])
+        precursor_range = PrecursorRange(expected_peptide_mass, 0, 0)
+        validator = PeptideMassValidator(modification_collection, 3, precursor_range)
+        self.assertTrue(validator.validate(peptide))
+
+        # Test variable n-terminal
+        # Variable c-terminal modification of R
+        # Static carbamidomethylation of C
+        # 2 variable oxidation of M
+        expected_peptide_mass = peptide.weight \
+            + variable_custom_modification_of_c_terminal_r.delta \
+            + peptide.c_count * static_carbamidomethylation_of_c.delta \
+            + 2 * variable_oxidation_of_m.delta
+        modification_collection = ModificationCollection([static_carbamidomethylation_of_c, variable_oxidation_of_m, variable_custom_modification_of_c_terminal_r])
+        precursor_range = PrecursorRange(expected_peptide_mass, 0, 0)
+        validator = PeptideMassValidator(modification_collection, 3, precursor_range)
+        self.assertTrue(validator.validate(peptide))
+
+        # This should fail with only 2 allowed variable modifications
+        validator.set_maximum_number_of_variable_modifications(2)
+        self.assertFalse(validator.validate(peptide))
+
+
+        # Test static n-terminal modification
+        # Static c-terminal modification of R
+        # Static carbamidomethylation of C
+        # 2 variable oxidation of M
+        expected_peptide_mass = peptide.weight \
+            + static_custom_modification_of_c_terminal_r.delta \
+            + peptide.c_count * static_carbamidomethylation_of_c.delta \
+            + 2 * variable_oxidation_of_m.delta
+        modification_collection = ModificationCollection([static_carbamidomethylation_of_c, variable_oxidation_of_m, static_custom_modification_of_c_terminal_r])
+        precursor_range = PrecursorRange(expected_peptide_mass, 0, 0)
+        validator = PeptideMassValidator(modification_collection, 3, precursor_range)
+        self.assertTrue(validator.validate(peptide))
---
+++ trypperdb/peptide_mass_validator.py
@@ -0,0 +1,134 @@
+from .proteomics.modification_collection import ModificationCollection
+from .proteomics.modification import Modification
+from .proteomics.mass.precursor_range import PrecursorRange
+
+class PeptideMassValidator:
+    def __init__(self, modification_collection: ModificationCollection, maximum_number_of_variable_modifications: int, precursor_range: PrecursorRange):
+        self.__modification_collection = modification_collection
+        self.__maximum_number_of_variable_modifications = maximum_number_of_variable_modifications
+        self.__precursor_range = precursor_range
+        self.__static_modifications = {}
+        self.__variable_modifications = {}
+        self.__variable_n_terminus_modifications = {}
+        self.__variable_c_terminus_modifications = {}
+
+        # Create lookup table for static non-terminus modifications
+        for modification in self.__modification_collection.static:
+            if not modification.is_terminus_modification:
+                self.__static_modifications[modification.amino_acid.one_letter_code] = modification
+
+        # Create lookup tables for variable modifications
+        for modification in self.__modification_collection.variable:
+            if not modification.is_terminus_modification:
+                self.__variable_modifications.setdefault(modification.amino_acid.one_letter_code, []).append(modification)
+            elif modification.is_position_n_terminus:
+                self.__variable_n_terminus_modifications.setdefault(modification.amino_acid.one_letter_code, []).append(modification)
+            elif modification.is_position_c_terminus:
+                self.__variable_c_terminus_modifications.setdefault(modification.amino_acid.one_letter_code, []).append(modification)
+
+        # Set to actual values in self.validate()
+        self.__peptide = None
+        self.__applied_modifications = None
+
+    def set_precursor_range(self, precursor_range: PrecursorRange):
+        """
+        Set new precursor range
+        @param precursor_range Instance of PrecursorRange
+        """
+        self.__precursor_range = precursor_range
+
+    def set_maximum_number_of_variable_modifications(self, maximum_number_of_variable_modifications: int):
+        """
+        Sets a new limit for variable modifications
+        @param maximum_number_of_variable_modifications New limit
+        """
+        self.__maximum_number_of_variable_modifications = maximum_number_of_variable_modifications
+
+    def __current_peptide_mass(self) -> int:
+        """
+        Returns the current peptide weight inclusively the applied modifications
+        @return int Returns the mass of the modified peptide
+        """
+        modifications_delta_sum = sum(modification.delta for modification in self.__applied_modifications if modification)
+        return self.__peptide.weight + modifications_delta_sum 
+
+    def __validate(self, current_mod_idx: int, number_of_variable_modifications: int) -> bool:
+        """
+        Recursivly try all variable modifications until 
+        @param current_mod_idx Current modification index
+        @param number_of_variable_modifications Current number of variable modifications
+        @return bool True if the peptide + a combindation of modifications matches the precursor range 
+        """
+        # Return True if weight matches
+        if self.__current_peptide_mass() in self.__precursor_range:
+            return True
+        # Return False if index is out of bounds
+        if current_mod_idx == len(self.__applied_modifications):
+            return False
+        # Skip modification adjustments if current modification is static and go to next 
+        if self.__applied_modifications[current_mod_idx] and self.__applied_modifications[current_mod_idx].is_static:
+            return self.__validate(current_mod_idx + 1, number_of_variable_modifications)
+        # Return false if no more variable modification is allowed
+        if number_of_variable_modifications == self.__maximum_number_of_variable_modifications:
+            return False
+
+        # Try all variable non-terminus modification
+        if 0 < current_mod_idx < len(self.__applied_modifications) - 1:
+            amino_acid = self.__peptide.sequence[current_mod_idx - 1]
+            for modification in self.__variable_modifications.get(amino_acid, []):
+                self.__applied_modifications[current_mod_idx] = modification
+                if self.__validate(current_mod_idx + 1, number_of_variable_modifications + 1):
+                    return True
+        # Try all variable n-terminus modification
+        elif current_mod_idx == 0:
+            amino_acid = self.__peptide.sequence[0]
+            for modification in self.__variable_n_terminus_modifications.get(amino_acid, []):
+                self.__applied_modifications[current_mod_idx] = modification
+                if self.__validate(current_mod_idx + 1, number_of_variable_modifications + 1):
+                    return True
+        # Try all variable c-terminus modifications
+        elif current_mod_idx == len(self.__applied_modifications) - 1:
+            amino_acid = self.__peptide.sequence[-1]
+            for modification in self.__variable_c_terminus_modifications.get(amino_acid, []):
+                self.__applied_modifications[current_mod_idx] = modification
+                if self.__validate(current_mod_idx + 1, number_of_variable_modifications + 1):
+                    return True
+
+        # Remove this variable modification
+        self.__applied_modifications[current_mod_idx] = None
+        # Try variable modification on next amino acid
+        return self.__validate(current_mod_idx + 1, number_of_variable_modifications)
+
+
+
+    def validate(self, peptide) -> bool:
+        """
+        Checks if the given peptide can be modified to match the precursor range.
+        @param peptide Object based on PeptideBase
+        @return boolean True if the peptide + a combination of modification matches the precursor, if not False
+        """
+
+        # Set peptide
+        self.__peptide = peptide
+        self.__applied_modifications = [None] * (peptide.length + 2) # +2 for n- and c-terminus
+
+        # Set static modifications
+        for idx, amino_acid in enumerate(peptide.sequence):
+            self.__applied_modifications[idx + 1] = self.__static_modifications.get(amino_acid, None)
+
+        if self.__modification_collection.static_n_terminus_modifications:
+            self.__applied_modifications[0] = self.__modification_collection.static_n_terminus_modifications
+
+        if self.__modification_collection.static_c_terminus_modifications:
+            self.__applied_modifications[-1] = self.__modification_collection.static_c_terminus_modifications
+
+        is_valid = self.__validate(0, 0)
+
+        # Reset variables
+        self.__peptide = None
+        self.__applied_modifications = None
+        self.__number_of_variable_modifications = 0
+
+        return is_valid
+
+
