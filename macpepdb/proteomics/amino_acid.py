801376965c9f95b72d44d550495196f8510a2e26
Dirk Winkelhardt <dirk.winkelhardt@rub.de>
2021-03-22 13:57:18 +0000

    remove commented amino acids for average and shared masses

--- macpepdb/proteomics/amino_acid.py
+++ macpepdb/proteomics/amino_acid.py
@@ -36,10 +36,10 @@ class AminoAcid:
         return X
 
 
+
+# Standard amino acids
 # https://proteomicsresource.washington.edu/protocols06/masses.php
 A = AminoAcid("Alanine", 'A', "Ala", "C3H5ON", 71.037113805, 71.0788, 9.1)
-# B is an average between N and D
-# B = AminoAcid("Asparagine or aspartic acid", 'B', "Asx", "", 114.53495, 114.5962, 9.2)
 C = AminoAcid("Cysteine", 'C', "Cys", "C3H5ONS", 103.009184505, 103.1388, 1.1)
 D = AminoAcid("Aspartic acid", 'D', "Asp", "C4H5O3N", 115.026943065, 115.0886, 5.4)
 E = AminoAcid("Glutamic acid", 'E', "Glu", "C5H7O3N", 129.042593135, 129.1155, 6.1)
@@ -47,7 +47,6 @@ F = AminoAcid("Phenylalanine", 'F', "Phe", "C9H9ON", 147.068413945, 147.1766, 3.
 G = AminoAcid("Glycine", 'G', "Gly", "C2H3ON", 57.021463735, 57.0519, 7.3)
 H = AminoAcid("Histidine", 'H', "His", "C6H7ON3", 137.058911875, 137.1411, 2.1)
 I = AminoAcid("Isoleucine", 'I', "Ile", "C6H11ON", 113.084064015, 113.1594, 5.6)
-#J = AminoAcid("Isoleucine or Leucine", 'J', "Ile or Leu", "C6H11ON", 113.084064015, 113.1594, 15.4)
 K = AminoAcid("Lysine", 'K', "Lys", "C6H12ON2", 128.094963050, 128.1741, 4.9)
 L = AminoAcid("Leucine", 'L', "Leu", "C6H11ON", 113.084064015, 113.1594, 9.8)
 M = AminoAcid("Methionine", 'M', "Met", "C5H9ONS", 131.040484645, 131.1926, 2.3)
@@ -61,8 +60,7 @@ T = AminoAcid("Threonine", 'T', "Thr", "C4H7O2N", 101.047678505, 101.1051, 5.5)
 U = AminoAcid("Selenocysteine", 'U', "SeC", "C3H5NOSe", 150.953633405, 150.0379, 0.0)
 V = AminoAcid("Valine", 'V', "Val", "C5H9ON", 99.068413945, 99.1326, 6.8)
 W = AminoAcid("Tryptophan", 'W', "Trp", "C11H10ON2", 186.079312980, 186.2132, 1.3)
-# Some Search Engines and Databases used the X Amino Acid for unknown amino acids
-X = AminoAcid("Unknown Amino Acid", 'X', "Xaa", "Unknown", 0.0, 0.0, 0.0)
 Y = AminoAcid("Tyrosine", 'Y', "Tyr", "C9H9O2N", 163.063328575, 163.1760, 2.9)
-# Z is an average btween E and Q
-# Z = AminoAcid("Glutamine or glutamic acid", 'Z', "Glx", "", 128.55059, 128.6231, 9.8)
+# Special amino acids
+## Some Search Engines and Databases used the X Amino Acid for unknown amino acids
+X = AminoAcid("Unknown Amino Acid", 'X', "Xaa", "Unknown", 0.0, 0.0, 0.0)
