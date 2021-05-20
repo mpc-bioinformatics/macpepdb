b64b3a0e1484d88db0d30ae42a17c25eae085b47
Dirk Winkelhardt <dirk.winkelhardt@rub.de>
2021-05-20 17:47:38 +0200

    use ColumnConditionsList for column conditions

--- macpepdb/models/modification_combination_list.py
+++ macpepdb/models/modification_combination_list.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 import pathlib
 import csv
+import operator
 from math import floor
+from typing import Tuple, List, Any
 
 from ..proteomics.modification_collection import ModificationCollection
 from ..proteomics.mass.precursor_range import PrecursorRange
+from .column_conditions_list import ColumnConditionsList
 
 # Only a simple helper class to build the combination matrix in ModifiedPeptideQueryFilter
 class ModificationCounter:
@@ -15,17 +18,17 @@ class ModificationCounter:
 class ModificationCombination:
     """
     This class keeps all information for querying peptides with a specific modification combination.
-    The property `precursor_range` holds the PrecursorRange (mass tolerance) for the query and `column_conditions` contains a dictionary,
-    where the key is the column name and the value is a tupel which keeps the operator and the value, e.g. `(operator, value)`
+    The property `precursor_range` holds the PrecursorRange (mass tolerance) for the query and `column_conditions_list` contains a ColumnConditionsList
+    to check the values of SQL row for additional conditions.
     """
     def __init__(self, modification_counters: list, precursor: int, lower_precursor_tolerance_ppm: int, upper_precursor_tolerance_ppm: int):
-        self.__column_conditions = {}
+        self.__column_conditions_list = ColumnConditionsList()
         self.__precursor_range = (0, 0)
         self.__calculate_columns_and_precursor_range(modification_counters, precursor, lower_precursor_tolerance_ppm, upper_precursor_tolerance_ppm)
 
     @property
-    def column_conditions(self):
-        return self.__column_conditions
+    def column_conditions_list(self) -> ColumnConditionsList:
+        return self.__column_conditions_list
 
     @property
     def precursor_range(self):
@@ -61,38 +64,49 @@ class ModificationCombination:
             # Build the column name, e.g. a_count
             column_name = "{}_count".format(one_letter_code.lower())
             # Add condition. In case the modification is fix we want equals the amino acid, if the modification is variable it could be more.
-            self.__column_conditions[column_name] = (
-                '=' if count_and_type[1] else '>=',
+            sql_representation, func_operator = ("= %s", operator.eq) if count_and_type[1] else (">= %s", operator.ge)
+            self.__column_conditions_list.append(
+                column_name,
+                sql_representation,
+                func_operator,
                 count_and_type[0]
             )
 
         # For n- and c-terminus modification check if there is one:
         # * If the modification is variable and applied check for presence (absence is uninteresting because it is a variable modification)
         if n_terminus_modification and (n_terminus_modification[0].is_variable and n_terminus_modification[1]):
-            self.__column_conditions['n_terminus'] = (
-                '=',
+            self.__column_conditions_list.append(
+                'n_terminus',
+                "= %s",
+                operator.eq,
                 n_terminus_modification[0].amino_acid.one_letter_code
             )
 
         if c_terminus_modification and (c_terminus_modification[0].is_variable and c_terminus_modification[1]):
-            self.__column_conditions['c_terminus'] = (
-                '=',
+            self.__column_conditions_list.append(
+                'c_terminus',
+                "= %s",
+                operator.eq,
                 c_terminus_modification[0].amino_acid.one_letter_code
             )
 
         # Add the mass between condition
         self.__precursor_range = PrecursorRange(precursor - delta_sum, lower_precursor_tolerance_ppm, upper_precursor_tolerance_ppm)
 
-    def to_sql(self) -> tuple:
+    def to_sql(self) -> Tuple[str, List[Any]]:
+        """Returns the SQL where condition for this combination.
+        """
         # Begin with mass
-        query_string = "mass BETWEEN %s AND %s"
-        values = [self.precursor_range.lower_limit, self.precursor_range.upper_limit]
-        # Add each additional column condition
-        for column_name, (operator, value) in self.column_conditions.items():
-            query_string += " AND "
-            query_string += f"{column_name} {operator} %s"
-            values.append(value)
-        return (query_string, values)
+        where_string = "mass BETWEEN %s AND %s"
+        where_values = [self.precursor_range.lower_limit, self.precursor_range.upper_limit]
+        # Check len of column_conditions 
+        if len(self.__column_conditions_list):
+            # Create string and values from the condition list
+            additionl_conditions, additional_condition_values = self.__column_conditions_list.to_sql()
+            where_string += f" AND {additionl_conditions}"
+            where_values += additional_condition_values
+        return (where_string, where_values)
+
 
 class ModificationCombinationList:
     """
