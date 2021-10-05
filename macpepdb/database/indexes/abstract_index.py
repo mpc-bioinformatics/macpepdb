52b25609c892e0356a37532868c0a6b9440e63da
Dirk Winkelhardt <dirk.winkelhardt@rub.de>
2021-10-05 09:26:30 +0000

    add class which reflacts database indexes

---
+++ macpepdb/database/indexes/abstract_index.py
@@ -0,0 +1,37 @@
+import sys
+import itertools
+from copy import deepcopy
+from dataclasses import dataclass
+from macpepdb.database.indexes.column_definition import ColumnDefinition
+from typing import ClassVar, List
+
+@dataclass
+class AbstractIndex:
+    """
+    This class and its subclasses should provide a order for columns used in the represented database index.
+    This class can than be used to order columns in a where clause so the acutal query uses the index.
+    """
+    COLUMN_CONDITIONS_TEMPLATE: ClassVar[List[ColumnDefinition]] = []
+
+    @classmethod
+    def get_column_index(cls, column_name: str) -> int:
+        """
+        Returns the index of the column within the index.
+
+        Arguments
+        =========
+        column_name : str
+            Name of the column to search for.
+
+        Return
+        ======
+        Returns the column index.
+
+        Raise
+        =====
+        Raises ValueError when the column was not found
+        """
+        for column_index, column_condition in enumerate(cls.COLUMN_CONDITIONS_TEMPLATE):
+            if column_condition.column_name == column_name:
+                return column_index
+        raise ValueError
---
+++ macpepdb/database/indexes/column_definition.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from typing import Any, Tuple
+
+@dataclass
+class ColumnDefinition:
+    """
+    Defines the column name, default operator and values to build a query which uses a specific index.
+    """
+    __slots__ = ["__column_name", "__operator", "__values"]
+    __column_name: str
+    __operator: str
+    __values: Any
+
+    def __init__(self, column_name: str, operator: str, values: Tuple[Any]):
+        self.__column_name = column_name
+        self.__operator = operator
+        self.__values = values
+
+    @property
+    def column_name(self) -> str:
+        return self.__column_name
+
+    @property
+    def operator(self) -> str:
+        return self.__operator
+    
+    @property
+    def values(self) -> Tuple[Any]:
+        return self.__values
+
+    def get_sql_definition(self) -> str:
+        """
+        Put column name and operation together
+
+        Return
+        ======
+        E.g. "mass BETWEEN %s AND %s" when column name was "mass" and operator was "BETWEEN %s AND %s".
+        """
+        return f"{self.__column_name} {self.__operator}"
